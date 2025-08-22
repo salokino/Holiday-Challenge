644d5f55dd414c9b4956a43928ee2ee1bff44e78
Nikolas Witt <nwitt.lpz@gmail.com>
2025-08-22 12:20:30 +0200

    add pydantic classes

---
+++ src/backend/schemas.py
@@ -0,0 +1,28 @@
+from datetime import date
+from pydantic import BaseModel, Field
+from typing import Annotated
+
+
+class HotelsSearchQueryBasic(BaseModel):
+    model_config = {"extra": "forbid"}
+
+    adults: Annotated[int | None, Field(ge=0)] = None
+    children: Annotated[int | None, Field(ge=0)] = None
+    duration: Annotated[int | None, Field(ge=0)] = None
+    earliest_departure: Annotated[date | None, Field()] = None
+    inbound_departure_airport: Annotated[str | None, Field(pattern="[A-Z]{3}")] = None
+    latest_return: Annotated[date | None, Field()] = None
+
+
+class HotelsSearchQueryAdvanced(HotelsSearchQueryBasic):
+    inbound_arrival_time: Annotated[str | None, Field(pattern="morning|afternoon|evening|night")] = None
+    inbound_departure_time: Annotated[str | None, Field(pattern="morning|afternoon|evening|night")] = None
+    mealtype: Annotated[str | None, Field(max_length=32)] = None
+    oceanview: Annotated[bool | None, Field()] = None
+    outbound_arrival_airport: Annotated[str | None, Field(pattern="[A-Z]{3}")] = None
+    outbound_arrival_time: Annotated[str | None, Field(pattern="morning|afternoon|evening|night")] = None
+    outbound_departure_time: Annotated[str | None, Field(pattern="morning|afternoon|evening|night")] = None
+    price_max: Annotated[int | None, Field(ge=0)] = None
+    price_min: Annotated[int | None, Field(ge=0)] = None
+    roomtype: Annotated[str | None, Field(max_length=32)] = None
+    
