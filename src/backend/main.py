4140729bebc1c7eb01a7ba9ce156e72d962f40c2
Nikolas Witt <nwitt.lpz@gmail.com>
2025-08-24 22:00:06 +0200

    use databaseclient

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,20 +1,18 @@
+from database import DatabaseClient
 from dotenv import dotenv_values, load_dotenv
 from fastapi import FastAPI, Depends
-from models import Offer
 from query_builder import QueryBuilder
-from schemas import HotelsSearchQueryAdvanced
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from schemas import HotelsSearchQueryAdvanced, HotelOfferResponse
+from sqlalchemy.orm import Session
 from typing import Annotated
-
-import datetime
 
 
 load_dotenv()
 config = dotenv_values(".env")
 
 app = FastAPI()
-
+db_client = DatabaseClient(url=f"postgresql://{config['USER']}:{config['PASSWORD']}"\
+                               f"@{config['HOST']}:{config['PORT']}/{config['DB_NAME']}")
 
 @app.get("/")
 def read_root():
@@ -25,21 +23,38 @@ def read_hotes():
     return {}
 
 @app.get("/hotels")
-def search_hotels(query_params: Annotated[HotelsSearchQueryAdvanced, Depends()]):
+def search_hotels(
+    query_params: Annotated[HotelsSearchQueryAdvanced, Depends()],
+    session: Session = Depends(db_client.get_session)
+    ) -> list[HotelOfferResponse]:
+    """
+    Endpoint to search for hotel offers based on various query parameters.
+    The endpoint returns the cheapest offer of each hotel along with the count of offers per hotel
+    and trip details.
+
+    Parameters
+    ----------
+    query_params : HotelsSearchQueryAdvanced
+        The query parameters for searching hotels.
+    session : Session
+        The SQLAlchemy session to use for the query.
+
+    Returns
+    -------
+    List[HotelOfferResponse]
+        A list of hotel offers matching the query parameters.
+    """
+
     query_builder = QueryBuilder(query_params=query_params)
     query = query_builder.build_query()
-
-    engine = create_engine(url=f"postgresql://{config['USER']}:{config['PASSWORD']}"\
-                               f"@{config['HOST']}:{config['PORT']}/{config['DB_NAME']}")
-
-    sm = sessionmaker(bind=engine)
-    session = sm()
-
     results = session.execute(query).all()
 
-    for result in results:
-       print(result)
-       print("~~~~~~~~~~~~----------------------------------------------~~~~~~~~~~~~")
+    response_objects = []
 
-    print(len(results))
+    # convert results to response objects
+    for res in results:
+        res = res._asdict()
+        response_obj = HotelOfferResponse.model_validate(res)
+        response_objects.append(response_obj)
 
+    return response_objects
